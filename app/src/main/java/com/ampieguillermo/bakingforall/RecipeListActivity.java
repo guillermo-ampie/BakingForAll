e237c7f589f86f9189e2d1316c565e26d37f01ce
Guillermo Ampie <ga.ampie@gmail.com>
2018-08-11 01:20:08 -0500

    Parse JSON file

--- app/src/main/java/com/ampieguillermo/bakingforall/RecipeListActivity.java
+++ app/src/main/java/com/ampieguillermo/bakingforall/RecipeListActivity.java
@@ -9,7 +9,18 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import com.ampieguillermo.bakingforall.dummy.DummyContent;
+import com.ampieguillermo.bakingforall.model.Recipe;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 /**
  * An activity representing a list of Recipes. This activity
@@ -54,6 +65,7 @@ public class RecipeListActivity extends AppCompatActivity {
     final RecyclerView recyclerView = findViewById(R.id.recyclerview_recipe_list);
     assert recyclerView != null;
     setupRecyclerView(recyclerView);
+    loadJsonData();
   }
 
   private void setupRecyclerView(@NonNull final RecyclerView recyclerView) {
@@ -68,4 +80,19 @@ public class RecipeListActivity extends AppCompatActivity {
     recyclerView.setAdapter(new SimpleItemAdapter(this, DummyContent.ITEMS, mTwoPane));
   }
 
+  // TODO: 8/11/18 Move this to an AsyncTaskLoader
+  private void loadJsonData() {
+    final Gson gson = new Gson();
+
+    try (InputStream inputStream = getAssets().open("baking.json")) {
+      final Reader reader = new InputStreamReader(inputStream, Charset.defaultCharset());
+      final ArrayList<Recipe> recipeList =
+          gson.fromJson(reader, new TypeToken<ArrayList<Recipe>>() {}.getType());
+    } catch (final FileNotFoundException e) {
+      Log.e(LOG_TAG, String.format("File not found: %s", e.getLocalizedMessage()));
+    } catch (final IOException e) {
+      Log.e(LOG_TAG, String.format("I/O Error: %s", e.getLocalizedMessage()));
+    }
+  }
 }
+
