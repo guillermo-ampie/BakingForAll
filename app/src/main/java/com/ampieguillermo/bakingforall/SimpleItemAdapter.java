738342babae30fbf03ffcd61e966dfac3948e016
Guillermo Ampie <ga.ampie@gmail.com>
2018-08-11 03:15:23 -0500

    Refactor SimpleItemAdapter: - Extends from ListAdapter<T, VH> instead of Adapter<VH>

--- app/src/main/java/com/ampieguillermo/bakingforall/SimpleItemAdapter.java
+++ app/src/main/java/com/ampieguillermo/bakingforall/SimpleItemAdapter.java
@@ -4,20 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import com.ampieguillermo.bakingforall.SimpleItemAdapter.SimpleItemViewHolder;
-import com.ampieguillermo.bakingforall.dummy.DummyContent.DummyItem;
+import com.ampieguillermo.bakingforall.model.Recipe;
 import java.util.List;
+import java.util.Objects;
 
-public class SimpleItemAdapter
-    extends RecyclerView.Adapter<SimpleItemViewHolder> {
+public class SimpleItemAdapter extends ListAdapter<Recipe, SimpleItemViewHolder> {
+
+  private static final String LOG_TAG = SimpleItemAdapter.class.getSimpleName();
+  private static final DiffUtil.ItemCallback<Recipe> DIFF_CALLBACK =
+      new DiffUtil.ItemCallback<Recipe>() {
+        @Override
+        public boolean areItemsTheSame(Recipe oldItem, Recipe newItem) {
+          return Objects.equals(oldItem.getId(), newItem.getId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(Recipe oldItem, Recipe newItem) {
+          return ((Integer) oldItem.getId()).equals(newItem.getId());
+        }
+      };
 
   /* package */ final RecipeListActivity mParentActivity;
-  /* package */ final List<DummyItem> mValues;
   /* package */ final boolean mTwoPane;
   private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
     @Override
@@ -25,7 +40,8 @@ public class SimpleItemAdapter
       final int position = (int) view.getTag();
       if (mTwoPane) {
         final Bundle arguments = new Bundle();
-        arguments.putString(RecipeDetailFragment.ARG_ITEM_ID, mValues.get(position).id);
+        arguments.putString(RecipeDetailFragment.ARG_ITEM_ID,
+            Integer.toString(getItem(position).getId()));
         final RecipeDetailFragment fragment = new RecipeDetailFragment();
         fragment.setArguments(arguments);
         mParentActivity
@@ -36,7 +52,8 @@ public class SimpleItemAdapter
       } else {
         final Context context = view.getContext();
         final Intent intent = new Intent(context, RecipeDetailActivity.class);
-        intent.putExtra(RecipeDetailFragment.ARG_ITEM_ID, mValues.get(position).id);
+        intent.putExtra(RecipeDetailFragment.ARG_ITEM_ID,
+            Integer.toString(getItem(position).getId()));
 
         context.startActivity(intent);
       }
@@ -44,11 +61,14 @@ public class SimpleItemAdapter
   };
 
   SimpleItemAdapter(RecipeListActivity parent,
-      List<DummyItem> items,
       boolean twoPane) {
-    mValues = items;
+    super(DIFF_CALLBACK);
     mParentActivity = parent;
     mTwoPane = twoPane;
+  }
+
+  public void setItemList(final List<Recipe> itemList) {
+    submitList(itemList);
   }
 
   @NonNull
@@ -61,27 +81,25 @@ public class SimpleItemAdapter
 
   @Override
   public void onBindViewHolder(@NonNull final SimpleItemViewHolder holder, int position) {
-    holder.mIdView.setText(mValues.get(position).id);
-    holder.mContentView.setText(mValues.get(position).content);
-
-    holder.itemView.setTag(position);
+    holder.setupItemView(getItem(position));
   }
 
-  @Override
-  public int getItemCount() {
-    return mValues.size();
-  }
+  /* package */ static class SimpleItemViewHolder extends RecyclerView.ViewHolder {
 
-    /* package */ static class SimpleItemViewHolder extends RecyclerView.ViewHolder {
+    private final TextView mIdView;
+    private final TextView mContentView;
 
-    /* package */ final TextView mIdView;
-    /* package */ final TextView mContentView;
-
-    SimpleItemViewHolder(View view, final View.OnClickListener listener) {
+    SimpleItemViewHolder(final View view, final View.OnClickListener listener) {
       super(view);
       mIdView = view.findViewById(R.id.textview_recipe_list_id);
       mContentView = view.findViewById(R.id.textview_recipe_list_content);
       itemView.setOnClickListener(listener);
+    }
+
+    void setupItemView(final Recipe recipe) {
+      mIdView.setText(String.format("<%d> ", recipe.getId()));
+      mContentView.setText(recipe.getName());
+      itemView.setTag(getAdapterPosition());
     }
   }
 }
