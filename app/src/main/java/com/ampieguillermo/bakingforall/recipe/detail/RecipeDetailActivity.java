bae4dc63d5acc8fc85c30521e0f00f803d31371b
Guillermo Ampie <ga.ampie@gmail.com>
2018-08-16 15:31:28 -0500

    Eliminate the FAB code

--- app/src/main/java/com/ampieguillermo/bakingforall/recipe/detail/RecipeDetailActivity.java
+++ app/src/main/java/com/ampieguillermo/bakingforall/recipe/detail/RecipeDetailActivity.java
@@ -3,8 +3,6 @@ package com.ampieguillermo.bakingforall.recipe.detail;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -38,13 +36,6 @@ public class RecipeDetailActivity extends AppCompatActivity {
     setContentView(R.layout.activity_recipe_detail);
     final Toolbar toolbar = findViewById(R.id.toolbar_recipe_detail);
     setSupportActionBar(toolbar);
-
-    final FloatingActionButton fab = findViewById(R.id.fab_recipe_detail);
-    fab.setOnClickListener(
-        view -> Snackbar.make(view,
-            "Replace with your own detail action",
-            Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show());
 
     // Show the Up button in the action bar.
     final ActionBar actionBar = getSupportActionBar();
