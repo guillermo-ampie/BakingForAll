5ecf9eec0f784b03925af2ec55e31b39d65ba44a
Guillermo Ampie <ga.ampie@gmail.com>
2018-09-17 16:02:15 -0500

    - Use DataBinding library - Add ExoPlayer to "RecipeStepContentFragment"

--- app/src/main/java/com/ampieguillermo/bakingforall/recipe/detail/recipestepcontent/RecipeStepContentFragment.java
+++ app/src/main/java/com/ampieguillermo/bakingforall/recipe/detail/recipestepcontent/RecipeStepContentFragment.java
@@ -1,15 +1,30 @@
 package com.ampieguillermo.bakingforall.recipe.detail.recipestepcontent;
 
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import com.ampieguillermo.bakingforall.R;
+import com.ampieguillermo.bakingforall.databinding.FragmentRecipeStepContentBinding;
 import com.ampieguillermo.bakingforall.model.RecipeStep;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.util.Util;
 import java.util.Objects;
+import okhttp3.OkHttpClient;
+import org.apache.commons.lang3.StringUtils;
 import org.parceler.Parcels;
 
 /**
@@ -18,7 +33,15 @@ import org.parceler.Parcels;
  */
 public class RecipeStepContentFragment extends Fragment {
 
+  private FragmentRecipeStepContentBinding binding;
+  private SimpleExoPlayer mExoPlayer;
+
+  /**
+   * Mandatory empty constructor for the fragment manager to instantiate the
+   * fragment (e.g. upon screen orientation changes).
+   */
   public RecipeStepContentFragment() {
+    // Mandatory empty constructor
   }
 
   public static RecipeStepContentFragment newInstance(final RecipeStep recipeStep) {
@@ -33,8 +56,9 @@ public class RecipeStepContentFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    final View rootView =
-        inflater.inflate(R.layout.fragment_recipe_step_content, container, false);
+
+    // Setup the DataBinding access
+    binding = FragmentRecipeStepContentBinding.inflate(inflater, container, false);
 
     if (Objects.requireNonNull(getArguments())
         .containsKey(RecipeStep.ARGUMENT_SELECTED_RECIPE_STEP)) {
@@ -50,11 +74,93 @@ public class RecipeStepContentFragment extends Fragment {
         if (appBarLayout != null) {
           appBarLayout.setTitle(recipeStep.getShortDescription());
         }
-        final TextView textViewDescription =
-            rootView.findViewById(R.id.textview_recipe_step_content_long_description);
-        textViewDescription.setText(recipeStep.getDescription());
+
+        binding.textviewRecipeStepContentLongDescription.setText(recipeStep.getDescription());
+
+        final String recipeStepVideoUrl = recipeStep.getVideoUrl();
+        if (StringUtils.isEmpty(recipeStepVideoUrl)) {
+          // There is no video for this recipe step
+          showNoVideo();
+        } else {
+          // Play video!
+          final Uri recipeStepVideoUri = Uri.parse(recipeStepVideoUrl);
+          setupExoPlayer(recipeStepVideoUri);
+        }
+      } else {
+        showNoVideo();
       }
+    } else {
+      showNoVideo();
     }
-    return rootView;
+    return binding.getRoot();
+  }
+
+  /**
+   * Called when the fragment is no longer in use.  This is called
+   * after {@link #onStop()} and before {@link #onDetach()}.
+   */
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+
+    releasePlayer();
+  }
+
+  private void showNoVideo() {
+    binding.playerviewRecipeStepContent.setVisibility(View.INVISIBLE); // Hide ExoPlayer
+    binding.imageviewRecipeStepContent.setVisibility(View.VISIBLE); // Show a default image
+  }
+
+  private void setupExoPlayer(final Uri recipeStepVideoUri) {
+    // Note: ExoPlayer setup code from: https://google.github.io/ExoPlayer/guide.html
+
+//    if (mExoPlayer == null) {
+    // Create an instance of the ExoPlayer.
+
+    final Activity activity = Objects.requireNonNull(getActivity());
+    // 1. Create a default TrackSelector
+    final DefaultBandwidthMeter defaultBandwidthMeter = new DefaultBandwidthMeter();
+    final TrackSelection.Factory videoTrackSelectionFactory =
+        new AdaptiveTrackSelection.Factory(defaultBandwidthMeter);
+    final DefaultTrackSelector trackSelector =
+        new DefaultTrackSelector(videoTrackSelectionFactory);
+
+    // 2. Create the player
+    mExoPlayer = ExoPlayerFactory.newSimpleInstance(activity, trackSelector);
+
+    // 3. Bind the player to the view.
+    binding.playerviewRecipeStepContent.setPlayer(mExoPlayer);
+
+    // 4. Playing a media from the Internet (Uri):
+
+    // Produces DataSource instances through which media data is loaded.
+    // Note: Using ExoPlayer OkHttp extension: The OkHttp extension is an HttpDataSource
+    // implementation using Square's OkHttp(https://square.github.io/okhttp/).
+    final DataSource.Factory dataSourceFactory = new OkHttpDataSourceFactory(new OkHttpClient(),
+        Util.getUserAgent(activity, getString(R.string.app_name)),
+        defaultBandwidthMeter); // Measures bandwidth during playback. Can be null if not required.
+
+    // Make a a MediaSource to play
+    final MediaSource videoSource =
+        new ExtractorMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(recipeStepVideoUri);
+
+    // Prepare the player with the media source.
+    mExoPlayer.prepare(videoSource);
+
+    // Auto-play the video!
+    // TODO: 9/16/18 Handle a preference for autoPlay videos
+    // TODO: 9/16/18 Cache the videos!
+    mExoPlayer.setPlayWhenReady(true);
+//    }
+  }
+
+  private void releasePlayer() {
+    if (mExoPlayer != null) {
+      mExoPlayer.stop();
+      mExoPlayer.release();
+//      mExoPlayer = null;
+    }
   }
 }
+
