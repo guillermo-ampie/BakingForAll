b5e82b648802d6a8b66acae66968c02aaf499d06
Guillermo Ampie <ga.ampie@gmail.com>
2018-09-18 01:16:58 -0500

    - Reformat some code - Add some annotations

--- app/src/main/java/com/ampieguillermo/bakingforall/recipe/detail/IngredientListFragment.java
+++ app/src/main/java/com/ampieguillermo/bakingforall/recipe/detail/IngredientListFragment.java
@@ -3,6 +3,7 @@ package com.ampieguillermo.bakingforall.recipe.detail;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
@@ -44,8 +45,8 @@ public class IngredientListFragment extends Fragment {
   }
 
   @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
     final View rootView =
         inflater.inflate(R.layout.fragment_ingredient_list, container, false);
 
@@ -74,9 +75,7 @@ public class IngredientListFragment extends Fragment {
       @NonNull final RecyclerView recyclerViewIngredientList) {
 
     recyclerViewIngredientList.setHasFixedSize(true);
-
     final IngredientItemAdapter itemAdapter = new IngredientItemAdapter();
-
     itemAdapter.setItemList(recipe.getIngredients());
     recyclerViewIngredientList.setAdapter(itemAdapter);
   }
